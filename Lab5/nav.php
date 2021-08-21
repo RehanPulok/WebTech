033345b839aa3a8e580a293ff822cd44295c2c1d
RehanPulok <pulok.rehan.aiub@gmail.com>
2021-08-21 13:24:07 +0600

    Add files via upload
    LAb 5 uploaded

---
+++ Lab5/nav.php
@@ -0,0 +1,5 @@
+ <ul>
+        <li><a href="/webtech-main/Lab5/addProduct.php"> Add Product</a></li>
+        <li><a href="/webtech-main/Lab5/showAllProducts.php"> Show all Product</a></li>
+        <li><a href="/webtech-main/Lab5/searchProduct.php"> Search Product</a></li>
+</ul>
